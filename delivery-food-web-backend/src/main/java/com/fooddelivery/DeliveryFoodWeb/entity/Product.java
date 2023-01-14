315dde373bffd5be7c5b758d841776867976548e
Peter Lin <47198276+peterlin456@users.noreply.github.com>
2023-01-13 19:01:31 -0500

    entity class update

--- delivery-food-web-backend/src/main/java/com/fooddelivery/DeliveryFoodWeb/entity/Product.java
+++ delivery-food-web-backend/src/main/java/com/fooddelivery/DeliveryFoodWeb/entity/Product.java
@@ -1,4 +1,38 @@
 package com.fooddelivery.DeliveryFoodWeb.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "product")
+@Data
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @Column(name = "unit_price")
+    private BigDecimal unitPrice;
+
+    @Column(name = "quantity")
+    private int quantity;
+
+    @Column(name = "product_id")
+    private Long productId;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+
 }
--- delivery-food-web-backend/src/main/java/com/fooddelivery/DeliveryFoodWeb/entity/Restaurant.java
+++ delivery-food-web-backend/src/main/java/com/fooddelivery/DeliveryFoodWeb/entity/Restaurant.java
@@ -1,4 +1,36 @@
 package com.fooddelivery.DeliveryFoodWeb.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "list_restaurant")
+@Data
 public class Restaurant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long Id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "status")
+    private boolean active;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "resturant_address_id", referencedColumnName = "id")
+    private Address resturantAddress;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "restaurant")
+    private Set<ProductCategory> productCategories;
+
+
+
 }
